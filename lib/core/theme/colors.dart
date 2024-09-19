14819de3ed642d4ca95110062f3edd6ac41eb4ec
Haidy <haidyahmedelsamman@gmail.com>
2024-09-19 23:09:42 +0300

    - push to orgin development

---
+++ lib/core/theme/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class ColorManager{
+   static const Color mainBlue = Color.fromARGB(255, 88, 129, 192);
+}
---
+++ lib/gemini_chat_bot.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:gemini_chat_bot/core/routes/app_router.dart';
+import 'package:gemini_chat_bot/core/routes/routes.dart';
+import 'package:gemini_chat_bot/core/theme/colors.dart';
+
+class GeminiChatBot extends StatelessWidget {
+  final AppRouter appRouter;
+  const GeminiChatBot({super.key, required this.appRouter});
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+        designSize: const Size(375, 812),
+        minTextAdapt: true,
+        child: MaterialApp(
+          title: 'Gemini Chat Bot',
+          theme: ThemeData(
+            primaryColor: ColorManager.mainBlue,
+            scaffoldBackgroundColor: Colors.white,
+          ),
+          debugShowCheckedModeBanner: false,
+          initialRoute:Routes.homeScreen ,
+          onGenerateRoute: appRouter.generateRoute,
+        ));
+  }
+}
